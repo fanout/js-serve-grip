afc6391e290dfdc9c6bf270a772dc881a2b1fe3c
harmony7 <kats@fanout.io>
2020-08-22 15:35:14 +0900

    Simplified tooling so commonjs build uses tsc instead of rollup and tests use jest

---
+++ rollup.browser.config.js
@@ -0,0 +1,38 @@
+import nodeGlobals from 'rollup-plugin-node-globals';
+import nodeBuiltins from 'rollup-plugin-node-builtins';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/main.browser.ts',
+    output: {
+        file: 'browser/connect-grip.js',
+        format: 'iife',
+        name: 'ConnectGrip',
+    },
+    plugins: [
+        replace({
+            include: ['node_modules/jwt-simple/**'],
+            delimiters: ['', ''],
+            values: {
+                'crypto.createHmac': "require('create-hmac')",
+            },
+        }),
+        commonjs(),
+        nodeGlobals(),
+        nodeBuiltins(),
+        nodeResolve({
+            browser: true,
+        }),
+        json(),
+        typescript({
+            module: "es2015",
+            moduleResolution: "node",
+            sourceMap: false,
+            declaration: false,
+        }),
+    ],
+};
