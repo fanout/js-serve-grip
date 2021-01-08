9fcc08ba3469580a7c3bba17be302ee118c45653
harmony7 <kats@fanout.io>
2021-01-09 07:10:36 +0900

    Koa examples

---
+++ examples/koa-http/server.js
@@ -0,0 +1,57 @@
+const Koa = require( 'koa' );
+const Router = require( '@koa/router' );
+const KoaBody = require( 'koa-body' );
+const { ServeGrip } = require( '@fanoutio/serve-grip' );
+
+const PORT = 3000;
+const CHANNEL_NAME = 'test';
+const PUSHPIN_URL = "http://localhost:5561/";
+
+const app = new Koa();
+
+const serveGrip = new ServeGrip({
+    grip: {
+        control_uri: PUSHPIN_URL,
+    },
+});
+
+app.use(serveGrip.koa);
+
+const router = new Router();
+
+router.get('/api/stream', ctx => {
+
+    if (ctx.req.grip.isProxied) {
+
+        const gripInstruct = ctx.res.grip.startInstruct();
+        gripInstruct.addChannel(CHANNEL_NAME);
+        gripInstruct.setHoldStream();
+
+        ctx.set('Content-Type', 'text/plain');
+        ctx.body = '[stream open]\n';
+
+    } else {
+
+        ctx.set('Content-Type', 'text/plain');
+        ctx.body = "[not proxied]\n";
+
+    }
+
+});
+
+router.post('/api/publish', KoaBody(), async ctx => {
+
+    const data = ctx.request.body;
+
+    const publisher = serveGrip.getPublisher();
+    await publisher.publishHttpStream(CHANNEL_NAME, data + '\n');
+
+    ctx.set('Content-Type', 'text/plain');
+    ctx.body = 'Ok\n';
+
+});
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
---
+++ examples/koa-ws/server.js
@@ -0,0 +1,71 @@
+const Koa = require( 'koa' );
+const Router = require( '@koa/router' );
+const KoaBody = require( 'koa-body' );
+const { ServeGrip } = require( '@fanoutio/serve-grip' );
+const { WebSocketMessageFormat } = require( '@fanoutio/grip' );
+
+const PORT = 3000;
+const CHANNEL_NAME = 'test';
+const PUSHPIN_URL = "http://localhost:5561/";
+
+const app = new Koa();
+
+const serveGrip = new ServeGrip({
+    grip: {
+        control_uri: PUSHPIN_URL,
+    },
+});
+
+app.use(serveGrip.koa);
+
+const router = new Router();
+
+// Websocket-over-HTTP is translated to HTTP POST
+router.post('/api/websocket', ctx => {
+
+    const { wsContext } = ctx.req.grip;
+    if (wsContext == null) {
+        ctx.status = 400;
+        ctx.body = '[not a websocket request]\n';
+        return;
+    }
+
+    // If this is a new connection, accept it and subscribe it to a channel
+    if (wsContext.isOpening()) {
+        wsContext.accept();
+        wsContext.subscribe(CHANNEL_NAME);
+    }
+
+    while (wsContext.canRecv()) {
+        const message = wsContext.recv();
+
+        if (message == null) {
+            // If return value is undefined then connection is closed
+            wsContext.close();
+            break;
+        }
+
+        // Echo the message
+        wsContext.send(message);
+    }
+
+    // In Koa, specifically set the response body to null so that
+    // it doesn't return 404
+    ctx.body = null;
+
+});
+
+router.post('/api/broadcast', KoaBody(), async ctx => {
+
+    const publisher = serveGrip.getPublisher();
+    await publisher.publishFormats(CHANNEL_NAME, new WebSocketMessageFormat(ctx.request.body));
+
+    ctx.set('Content-Type', 'text/plain');
+    ctx.body = 'Ok\n';
+
+});
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
